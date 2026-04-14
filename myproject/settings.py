1d1b0f33edb44fac2e722b46bb58443f97457805
phuong <phuong@gmail.com>
2026-04-14 21:58:42 +0700

    fix allowed hosts

--- myproject/settings.py
+++ myproject/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-i&r&b*tvutw$@p^t1=4_kc46o7(&9q7vi=u9wqq#wjnkx^ojg5
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ["book-api-project-kdq1.onrender.com"]
 
 
 # Application definition
